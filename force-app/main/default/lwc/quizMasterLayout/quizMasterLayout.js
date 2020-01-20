ee174c6208ac5c3b7f866641b791b6e6bde3040b
Ganesh Shreedhar <Ganesh.Shreedhar@spartasystems.com>
2020-01-20 18:03:57 +0530

    adding wire to actual salesforce data

--- force-app/main/default/lwc/quizMasterLayout/quizMasterLayout.js
+++ force-app/main/default/lwc/quizMasterLayout/quizMasterLayout.js
@@ -1,3 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import QUESTION_FIELD from '@salesforce/schema/Question__c.Question__c';
 
-export default class QuizMasterLayout extends LightningElement {}
+export default class QuizMasterLayout extends LightningElement {
+    @wire(getRecord, { recordId: 'a006D000002Wk0lQAC', fields: [QUESTION_FIELD], optionalFields: [] })
+    questionRecord;
+
+     get question() {
+        return getFieldValue(this.questionRecord.data, QUESTION_FIELD);
+    }
+}
