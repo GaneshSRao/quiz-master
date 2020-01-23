059565265ae5a1dc8a4f71e91a9d3fb64887cb1c
Ganesh Shreedhar <Ganesh.Shreedhar@spartasystems.com>
2020-01-23 11:37:58 +0530

    adding questionCard check functianality

--- force-app/main/default/lwc/questionCard/questionCard.js
+++ force-app/main/default/lwc/questionCard/questionCard.js
@@ -1,13 +1,68 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 export default class QuestionCard extends LightningElement {
-    @api question;
-    @api answer1;
-    @api answer2;
-    @api answer3;
-    @api answer4;
-    @api answer5;
-    @api identifyCorrectAnswer;
+  @api question;
+  @api answer1;
+  @api answer2;
+  @api answer3;
+  @api answer4;
+  @api answer5;
+  @api identifycorrectanswer;
+  @api questiontype;
 
-    
+  @track selectedAnswer = [];
+
+  get isSingleSelect() {
+    return this.questiontype === "Single-Select";
+  }
+
+  handleSubmitClick() {
+    if (
+      this.selectedAnswer[0] === this.identifycorrectanswer ||
+      this.selectedAnswer.sort().toString() ===
+        this.identifycorrectanswer
+          .split(";")
+          .sort()
+          .toString()
+    ) {
+      const evt = new ShowToastEvent({
+        title: "",
+        message: "You have selected the Correct answer(s)",
+        variant: "success"
+      });
+      this.dispatchEvent(evt);
+    } else if (
+      this.selectedAnswer.length !==
+      this.identifycorrectanswer.split(";").length
+    ) {
+      const evt = new ShowToastEvent({
+        title: "",
+        message: "Select correct number of options",
+        variant: "warning"
+      });
+      this.dispatchEvent(evt);
+    } else {
+      const evt = new ShowToastEvent({
+        title: "",
+        message: "You have selected the Wrong answer(s)",
+        variant: "error"
+      });
+      this.dispatchEvent(evt);
+    }
+  }
+
+  handleRadioOptionsClick(event) {
+    this.selectedAnswer[0] = event.target.value;
+  }
+
+  handleCheckboxOptionsClick(event) {
+    if (event.target.checked) {
+      this.selectedAnswer.push(event.target.value);
+    } else {
+      this.selectedAnswer = this.selectedAnswer.filter(
+        value => value !== event.target.value
+      );
+    }
+  }
 }
--- force-app/main/default/lwc/quizMasterLayout/quizMasterLayout.js
+++ force-app/main/default/lwc/quizMasterLayout/quizMasterLayout.js
@@ -1,12 +1,41 @@
 import { LightningElement, wire } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import QUESTION_FIELD from '@salesforce/schema/Question__c.Question__c';
+import ANSWER1 from '@salesforce/schema/Question__c.Answer1__c';
+import ANSWER2 from '@salesforce/schema/Question__c.Answer2__c';
+import ANSWER3 from '@salesforce/schema/Question__c.Answer3__c';
+import ANSWER4 from '@salesforce/schema/Question__c.Answer4__c';
+import ANSWER5 from '@salesforce/schema/Question__c.Answer5__c';
+import IDENTIFY_CORRECT_ANSWER from '@salesforce/schema/Question__c.Identify_Correct_Answer__c';
+import QUESTION_TYPE from '@salesforce/schema/Question__c.Question_Type__c';
+
 
 export default class QuizMasterLayout extends LightningElement {
-    @wire(getRecord, { recordId: 'a006D000002Wk0lQAC', fields: [QUESTION_FIELD], optionalFields: [] })
+    @wire(getRecord, { recordId: 'a006D000002Wk0lQAC', fields: [QUESTION_FIELD,ANSWER1,ANSWER2,ANSWER3,ANSWER4,IDENTIFY_CORRECT_ANSWER,QUESTION_TYPE], optionalFields: [ANSWER5] })
     questionRecord;
 
      get question() {
         return getFieldValue(this.questionRecord.data, QUESTION_FIELD);
+    }
+    get answer1() {
+        return getFieldValue(this.questionRecord.data, ANSWER1);
+    }
+    get answer2() {
+        return getFieldValue(this.questionRecord.data, ANSWER2);
+    }
+    get answer3() {
+        return getFieldValue(this.questionRecord.data, ANSWER3);
+    }
+    get answer4() {
+        return getFieldValue(this.questionRecord.data, ANSWER4);
+    }
+    get answer5() {
+        return getFieldValue(this.questionRecord.data, ANSWER5);
+    }
+    get questiontype() {
+        return getFieldValue(this.questionRecord.data, QUESTION_TYPE);
+    }
+    get identifycorrectanswer() {
+        return getFieldValue(this.questionRecord.data, IDENTIFY_CORRECT_ANSWER);
     }
 }
