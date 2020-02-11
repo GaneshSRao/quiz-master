5db398f1582a8fdd2ac8cbc22fd64229f1659d97
Ganesh Shreedhar <Ganesh.Shreedhar@spartasystems.com>
2020-02-11 11:50:21 +0530

    adding dynamic title

--- force-app/main/default/lwc/quizMasterLayout/quizMasterLayout.js
+++ force-app/main/default/lwc/quizMasterLayout/quizMasterLayout.js
@@ -1,12 +1,20 @@
 import { LightningElement, wire, track } from "lwc";
 import { refreshApex } from "@salesforce/apex";
 import getRandomQuestion from "@salesforce/apex/QuizMasterController.getRandomQuestion";
+import getTotalQuestionsCount from "@salesforce/apex/QuizMasterController.getRandomQuestion";
 
 export default class QuizMasterLayout extends LightningElement {
   @wire(getRandomQuestion)
   questionRecord;
 
+  @wire(getTotalQuestionsCount)
+  totalQuesitons;
+
   @track currentQuestion;
+
+  getTitle(){
+    return "Question XX pf "+this.totalQuesitons;
+  }
 
   renderedCallback(){
     this.currentQuestion = this.questionRecord.data;
